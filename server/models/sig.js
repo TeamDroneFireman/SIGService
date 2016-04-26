c11d6370d1d538eebb0826b95af1869ef907d7a0
thomas_huchede <huchede.thomas@gmail.com>
2016-04-26 15:23:12 +0200

    Change port for local

--- server/models/sig.js
+++ server/models/sig.js
@@ -1,7 +1,7 @@
 module.exports = function(Sig) {
-  const USERSERVICE_URL = 'http://projetm2gla.istic.univ-rennes1.fr:12346/';
+
   Sig.beforeRemote('*', function(ctx, unused, next) {
-    Sig.app.datasources.auth
+    Sig.app.datasources.userService
     .checkAuth(ctx.req.headers.userid, ctx.req.headers.token,
         function (err, response) {
       if (err || response.error || response.id !== ctx.req.headers.token) {
@@ -13,6 +13,7 @@ module.exports = function(Sig) {
       }
     });
   });
+  
   Sig.disableRemoteMethod('deleteById', true);
   Sig.disableRemoteMethod('updateAll', true);
   Sig.disableRemoteMethod('createChangeStream', true);
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 var supertest = require('supertest');
 var expect = require('chai').expect;
 
-var server = supertest('http://localhost:3000/api/SIGs');
+var server = supertest('http://localhost:3004/api/SIGs');
 
 describe('Server health response', function () {
   it('should return 200 OK', function (done) {
