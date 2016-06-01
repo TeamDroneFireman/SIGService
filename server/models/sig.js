c1dd409775393574870d7dbae9f9bd37c17ec92e
Gweltaz GESTIN <gestin.gweltaz@gmail.com>
2016-06-01 15:38:05 +0200

    Ajout de la methode de push pour Drone/Mean/Image/SIG

--- server/models/sig.js
+++ server/models/sig.js
@@ -38,12 +38,12 @@ module.exports = function(Sig) {
           var sigExternService = Sig.app.dataSources.sigExternService;
           interventionService.findById(id, function (err, response) {
             if (err) throw err;
-            if (response.error) 
+            if (response.error)
               next('> response error: ' + response.error.stack);
             sigExternService.getSigListMock(response.location.address,
               function (err, response) {
                 if (err) throw err;
-                if (response.error) 
+                if (response.error)
                   next('> response error: ' + response.error.stack);
                 Sigs.push.apply(Sigs, response);
                 callback(null, Sigs);
@@ -76,6 +76,41 @@ module.exports = function(Sig) {
     error.statusCode = error.status = 404;
     error.code = 'MODEL_NOT_FOUND';
     cb(error);
+  }
+
+  Sig.afterRemote('create',function (ctx, unused, next) {
+    sendPushMessage(ctx.result, 'Sig/Create');
+    next();
+  });
+
+  Sig.afterRemote('upsert',function (ctx, unused, next) {
+    sendPushMessage(ctx.result, 'Sig/Update');
+    next();
+  });
+
+  Sig.afterRemote('updateAll',function (ctx, unused, next) {
+    sendPushMessage(ctx.result, 'Drone/Update');
+    next();
+  });
+
+  Sig.afterRemote('deleteById',function (ctx, unused, next) {
+    sendPushMessage(ctx.result, 'Sig/Delete');
+    next();
+  });
+
+  function sendPushMessage(sig,topic){
+    var pushMessage = {
+      idIntervention : sig.intervention,
+      idElement : sig.id,
+      timestamp : Date.now(),
+      topic : topic
+    };
+    var pushService = Sig.app.datasources.pushService;
+    pushService.create(pushMessage, function(err,data){
+      if (err) throw err;
+      if (data.error)
+        next('> response error: ' + err.error.stack);
+    });
   }
 
 };
